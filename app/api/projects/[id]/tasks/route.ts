c5c921d0cd06bc2cc5e2016cb26501f7bc9206b5
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-11-27 07:56:45 -0500

    fixing for deployment

--- app/api/projects/[id]/tasks/route.ts
+++ app/api/projects/[id]/tasks/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function POST(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: any }
 ) {
   try {
     const { params } = context;
