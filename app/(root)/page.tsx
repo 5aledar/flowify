12c2949760476fd831847196ad1cff0984cfec94
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-12-01 03:00:01 -0500

    push to deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,8 @@ const Home = async () => {
   return (
     <main className='p-16 h-[100vh] ' >
       <div className="h-[400px] flex m-auto flex-col max-w-[900px] gap-5 justify-center p-16 items-start opacity-90 rounded-2xl bg-gradient-to-t  from-pink-500 to-purple-600">
-        <h1 className='text-3xl font-bold'>Make your work flowless</h1>
+        <h1 className='text-3xl font-bold'>Simplify your workflow and get things done effortlessly. Designed for individuals</h1>
+        <p>Flowify streamlines task management and keeps your projects flowing no clutter, just productivity.</p>
         <Link href={'/sign-up'}>
         <Button>
           Get started
