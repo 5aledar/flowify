dc65f2f58cede87075808c3f3c8ef66941211399
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-12-05 04:09:59 -0500

    push to deploy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { ModeToggle } from './ModeButton'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
+import Notifications from './Notify'
 const Navbar = async () => {
   return (
     <nav className='flex justify-between fixed w-full bg-white dark:bg-[#09090B] items-center md:h-[45px] h-[40px]  md:px-12 px-6 z-50'>
@@ -20,6 +21,7 @@ const Navbar = async () => {
         </SignedOut>
         <SignedIn>
           <UserButton />
+          <Notifications />
         </SignedIn>
         <ModeToggle />
       </div>
